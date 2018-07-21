1cf805164d4ffe9da0544107659559c6478d6a82
Bhaktij Koli <bhaktijkoli121@gmail.com>
2018-07-22 03:39:49 +0530

    Updated home page, added welcome section

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -25,7 +25,45 @@
       </div>
     </div>
   </section>
-  <section class="space">
+  <section id="welcome">
+    <div class="container">
+      <div class="row">
+        <div class="text-center">
+          <h1>WELCOME TO ANTIRATTA</h1>
+          <h3>AWESOME SUCCESS WITH STUDENT</h3>
+          <p>
+            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-4">
+          <div class="welcome-icon">
+            <h3><i class="fa fa-graduation-cap" aria-hidden="true"></i>LEARN COUSES FIRST</h3>
+            <p>
+              Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia con- sequuntur magni dolores eos qui ratione
+            </p>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="welcome-icon">
+
+            <h3><i class="fa fa-tags" aria-hidden="true"></i>440 COURSES AVAILABLE</h3>
+            <p>
+              Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia con- sequuntur magni dolores eos qui ratione
+            </p>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="welcome-icon">
+            <h3><i class="fa fa-diamond" aria-hidden="true"></i>EVERYTHING YOU NEED</h3>
+            <p>
+              Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia con- sequuntur magni dolores eos qui ratione
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
   <section id="counter" data-parallax="scroll" data-image-src="{{asset('images/banner.png')}}">
     <div class="container">
