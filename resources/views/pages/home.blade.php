ca29e3a0d0c8ea672d5ebb551063ef6a64ac143d
Bhaktij Koli <bhaktijkoli121@gmail.com>
2018-07-22 02:44:58 +0530

    Updated hero content

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -17,8 +17,8 @@
       </div>
     </div>
   </div>
-  <section id="stutor">
-    <div class="container-fluid hero-content-colored">
+  <section id="stutor" class="hero-content-colored">
+    <div class="container">
       <div class="text-center">
         <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h3>
         <a class="btn btn-white">BECOME A STUTOR</a>
