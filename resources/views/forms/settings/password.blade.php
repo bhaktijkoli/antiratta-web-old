99ea8f578f5b47686c3e0f8aaeb2adb5f870eadc
Bhaktij Koli <bhaktijkoli121@gmail.com>
2018-08-03 21:57:56 +0530

    Added change password page

--- resources/views/forms/settings/password.blade.php
+++ resources/views/forms/settings/password.blade.php
@@ -1,5 +1,38 @@
 <div class="card">
   <div class="card-body">
-
+    <form id="form_personal-info">
+      <h5>Change password</h5>
+      <hr />
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="form-group">
+            <input type="password" class="form-control" id="current_password" name="current_password" placeholder="Current Password">
+            <p class="help-block"></p>
+          </div>
+        </div>
+        <div class="col-sm-12">
+          <div class="form-group">
+            <input type="password" class="form-control" id="new_password" name="new_password" placeholder="New Password">
+            <p class="help-block"></p>
+          </div>
+        </div>
+        <div class="col-sm-12">
+          <div class="form-group">
+            <input type="password" class="form-control" id="confirm_password" name="confirm_password" placeholder="Confirm Password">
+            <p class="help-block"></p>
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="form-group">
+            <button type="submit" name="login" class="btn btn-primary btn-block">Change</button>
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="form-group">
+            <button type="submit" name="login" class="btn btn-default btn-block">Cancel</button>
+          </div>
+        </div>
+      </div>
+    </form>
   </div>
 </div>
