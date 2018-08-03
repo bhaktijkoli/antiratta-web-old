5b6bf6e875d97299001d112bcdc0ccf0f0ed1b3a
Bhaktij Koli <bhaktijkoli121@gmail.com>
2018-08-03 21:59:48 +0530

    Added email change page

--- resources/views/forms/settings/email.blade.php
+++ resources/views/forms/settings/email.blade.php
@@ -1,5 +1,21 @@
 <div class="card">
   <div class="card-body">
-
+    <form id="form_personal-info">
+      <h5>Email</h5>
+      <hr />
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="form-group">
+            <input type="email" class="form-control" id="email" name="email" value="{{$user->email}}" disabled>
+            <p class="help-block"></p>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="form-group">
+            <button type="submit" name="login" class="btn btn-primary btn-block">Change</button>
+          </div>
+        </div>
+      </div>
+    </form>
   </div>
 </div>
