925d69d424311e351004b08071782c62a5c0db82
Bhaktij Koli <bhaktijkoli121@gmail.com>
2018-10-24 01:11:24 +0530

    Updated Navbar

--- resources/assets/js/app/Layout/Navbar.vue
+++ resources/assets/js/app/Layout/Navbar.vue
@@ -25,34 +25,55 @@
             <router-link class="nav-link" :to="{ name: 'aboutus' }">About Us</router-link>
           </li>
           <li class="nav-divider"></li>
-          <li class="nav-item">
+          <li class="nav-item" v-if="auth==null">
             <router-link class="nav-link" :to="{ name: 'login' }">Login</router-link>
           </li>
-        </ul>
+          <li class="dropdown nav-item" v-else>
+            <a href="#" class="profile-photo dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false">
+              <div class="profile-photo-small">
+                <img src="http://via.placeholder.com/60x60&text=Avatar" alt="Circle Image" class="rounded-circle img-fluid">
+              </div>
+              <div class="ripple-container"></div></a>
+              <div class="dropdown-menu dropdown-menu-right">
+                <h6 class="dropdown-header">{{auth.firstname + " " + auth.lastname}}</h6>
+                <a href="/admin" class="dropdown-item" v-if="auth.role > 0">Admin Dashboard</a>
+                <div class="dropdown-divider" v-if="auth.role > 0"></div>
+                <a href="" class="dropdown-item">My Courses</a>
+                <a href="" class="dropdown-item">Settings</a>
+                <div class="dropdown-divider"></div>
+                <a href="" class="dropdown-item">Sign out</a>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
-</template>
+    </nav>
+  </template>
 
-<script>
-export default {
-  mounted() {
-    if(this.transparent) {
-      $(this.$refs.nav).addClass("navbar-transparent");
-      $(window).scroll(event => {
-        if($(window).scrollTop()>70) {
-          $(this.$refs.nav).removeClass("navbar-transparent");
-        } else {
-          $(this.$refs.nav).addClass("navbar-transparent");
-        }
-      });
-    }
-  },
-  props: {
-    transparent: {
-      default: false,
-      type: Boolean,
+  <script>
+  export default {
+    mounted() {
+      if(this.transparent) {
+        $(this.$refs.nav).addClass("navbar-transparent");
+        $(window).scroll(event => {
+          if($(window).scrollTop()>70) {
+            $(this.$refs.nav).removeClass("navbar-transparent");
+          } else {
+            $(this.$refs.nav).addClass("navbar-transparent");
+          }
+        });
+      }
+    },
+    props: {
+      transparent: {
+        default: false,
+        type: Boolean,
+      }
+    },
+    computed: {
+      auth() {
+        return this.$store.state.auth;
+      }
     }
   }
-}
-</script>
+  </script>
